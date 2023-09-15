5959e4a15f577db30643b7aa86bcd93ae10eb81d
Becca Jellinek <becca.jellinek@datadoghq.com>
2023-09-14 20:20:29 -0400

    vendingcents

--- vendingcents.java
+++ vendingcents.java
@@ -1,66 +1,37 @@
-import java.util.Scanner;
-
-public vendingcents {
-    public final int GOLDEN_DOLLAR = 100;
-    public final int HALF_DOLLAR = 50;
-    public final int QUARTER = 25;
-    public final int DIME = 10;
-    public final int NICKEL = 5;
-    public final int PENNY = 1;
-
-    // public static void calculate(int balance) {
-    //     int numGoldenDollars;
-    //     int numHalfDollars;
-    //     int numQuarters;
-    //     int numDimes;
-    //     int numNickels;
-    //     int numPennies;
-
-    //     if (balance >= GOLDEN_DOLLAR) {
-    //         numGoldenDollars = balance / GOLDEN_DOLLAR;
-    //         // this is an example of recursion
-    //         calculate(balance % GOLDEN_DOLLAR);
-    //      } else if (balance >= HALF_DOLLAR) {
- 
-    //      } else if (balance >= QUARTER) {
- 
-    //      } else if (balance >= DIME) {
- 
-    //      } else if (balance >=NICKEL) {
-             
-    //      } else if (balance >= PENNY ) {
- 
-    //      } else {
-    //          System.out.println("Something went wrong");
-    //      }
-
-    //     //  return 
-    // }
-
+/**
+ * Given number in cents, return proper change
+ * @author @beccajellinek
+ * @author @tiffanyatkinson
+ */
+public class vendingcents {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in); 
-        int balance = in.nextInt();
-         
-       
+        String arg = args[0];
+        int amount = Integer.parseInt(arg);
+
+        // number of golden dollar coins
+        int golden = amount / 100;
+        amount = amount % 100;
+
+        // number of half dollar coins
+        int half = amount / 50;
+        amount = amount % 50;
+
+        // number of quarters
+        int quarter = amount / 25;
+        amount = amount % 25;
+
+        int dime = amount / 10;
+        amount = amount % 10;
+
+    
+        int nickel = amount / 5;
+
+        int penny = amount % 5;
+
+
+        String formatString = "%d golden dollar coins, %d half dollar coins, %d quarters, %d dimes, %d nickels, %d pennies\n";
+        System.out.printf(formatString, golden, half, quarter, dime, nickel, penny);
+        
     }
 
 }
-
-
-/**
- * [vendingcents.java] Vending machine change maker. On the command line accept an
-integer representing the number of cents currently inside of a vending machine. (For
-example: java vendingcents 35, would mean that there are $0.35 inside the machine.)
-
-Return all of the money to the user using the largest possible change. (For example,
-with $0.65, we do not return 65 pennies, we return 2 quarters and a dime and a nickel.)
-
-java vendingcents 65
-"2 quarters, 1 dime, 1 nickel"
-
-
-The denominations you are using are golden dollar coin ($1.00 or 100 cents), half dollar
-coin ($0.50), quarter ($0.25), dime ($0.10), nickel ($0.05), and penny ($0.01). HINT:
-Modulus.
- * 
- */
